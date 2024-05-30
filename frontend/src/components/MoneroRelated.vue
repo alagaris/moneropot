27df103ba344326f4533cf7077a78073e09cb973
alagaris <40080592+alagaris@users.noreply.github.com>
2024-05-30 11:09:38 -0500

    updated related

--- frontend/src/components/MoneroRelated.vue
+++ frontend/src/components/MoneroRelated.vue
@@ -10,19 +10,19 @@ export default {
 <template>
   <modal modal-id="MoneroRelated" title="Spend / Earn / Get Monero">
     <div class="bg-gray-200 p-5 my-3">
-      <a href="https://xmrmemes.com/" target="_blank"
-        class="block text-lg font-semibold border-b-2 border-gray-400 pb-2 mb-4">XMR Memes</a>
-      <p>Get tipped Monero (XMR) for making memes about Monero.</p>
+      <a href="https://reddit.com/r/monero" target="_blank"
+        class="block text-lg font-semibold border-b-2 border-gray-400 pb-2 mb-4">r/Monero</a>
+      <p>Reddit forum for monero.</p>
     </div>
     <div class="bg-gray-200 p-5 my-3">
-      <a href="https://monero.win/" target="_blank"
-        class="block text-lg font-semibold border-b-2 border-gray-400 pb-2 mb-4">Monero.win</a>
-      <p>Win Monero by guessing what the last digit of the next Monero block hash will be!</p>
+      <a href="https://monero.observer/" target="_blank"
+        class="block text-lg font-semibold border-b-2 border-gray-400 pb-2 mb-4">Monero Observer</a>
+      <p>Everything that moves in the Monero community.</p>
     </div>
     <div class="bg-gray-200 p-5 my-3">
-      <a href="https://localmonero.co/" target="_blank"
-        class="block text-lg font-semibold border-b-2 border-gray-400 pb-2 mb-4">Local Monero</a>
-      <p>Buy and sell Monero (XMR) online without ID verification.</p>
+      <a href="https://www.revuo-xmr.com/" target="_blank"
+        class="block text-lg font-semibold border-b-2 border-gray-400 pb-2 mb-4">Revuo Monero</a>
+      <p>Monero updates.</p>
     </div>
     <div class="bg-gray-200 p-5 my-3">
       <a href="https://ccs.getmonero.org/" target="_blank"
@@ -33,6 +33,12 @@ export default {
       <a href="https://monero.com/" target="_blank"
         class="block text-lg font-semibold border-b-2 border-gray-400 pb-2 mb-4">Monero.com</a>
       <p>Buy, use, sell, and learn about Monero, the leading private digital payment method.</p>
+    </div>
+    <div class="bg-gray-200 p-5 my-3">
+      <a href="https://featherwallet.org/" target="_blank"
+        class="block text-lg font-semibold border-b-2 border-gray-400 pb-2 mb-4">Feather: A Free Monero Desktop
+        Wallet</a>
+      <p>Feather is a free, open-source Monero wallet for Linux, Tails, Windows and macOS.</p>
     </div>
     <p class="text-center p-1 text-xs">Have a suggestion for a new link? Send a message in contact form.</p>
   </modal>
